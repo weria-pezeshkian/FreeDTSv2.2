dbae9ff1adb6e273e4db5d457d1a274fb2e46a53
weria-pezeshkian <weria.pezeshkian@gmail.com>
2024-02-11 19:56:21 +0100

    comment

--- dts_src/OpenEdgeEvolutionWithConstantVertex.cpp
+++ dts_src/OpenEdgeEvolutionWithConstantVertex.cpp
@@ -284,7 +284,8 @@ if(createorkill<0.5)
         double DE = (enew-eold);
 
 
-        if(double(NL)/double(NS+1)*exp(-m_Beta*DE)>thermal )
+        //if(double(NL)/double(NS+1)*exp(-m_Beta*DE)>thermal )
+        if(exp(-m_Beta*DE)>thermal )
         {
              (*glo_energy)=(*glo_energy)+DE;
         }
@@ -389,7 +390,8 @@ else  // if(createorkill<0.5)
 
         double DE = (enew-eold);
                
-        if(double(NS)/double(NL+1)*(exp(-m_Beta*DE)>thermal ))
+        //if(double(NS)/double(NL+1)*(exp(-m_Beta*DE)>thermal ))
+        if((exp(-m_Beta*DE)>thermal ))
         {
              (*glo_energy)=(*glo_energy)+DE;
         }
